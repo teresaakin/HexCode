e16ecd5c64b0223b40a46883613210c5eff68937
mikestaarr <mhykhelofficial@gmail.com>
2024-02-26 10:59:14 +0100

    minor change

--- app/js/likes.js
+++ app/js/likes.js
@@ -12,6 +12,8 @@ function renderFavs() {
 
       f.style.backgroundImage = `linear-gradient(to right, ${fav.colors[0]}, ${fav.colors[1]}, ${fav.colors[2]})`
       // f.style.width = '150px'
+
+      //just for testing purposes
       f.style.height = '100px'
 
       palettes.appendChild(f)
