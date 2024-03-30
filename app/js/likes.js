9261c5d5f55f7c36bda03dad25e254597e75b1c2
Teresa Akinbodun <akinbodt@mcmaster.ca>
2024-03-29 23:06:32 -0400

    updated styling on liked palette cards

--- app/js/likes.js
+++ app/js/likes.js
@@ -7,16 +7,51 @@ function renderFavs() {
     const palettes = document.querySelector('.liked-palettes__wrapper')
 
     favs.forEach(fav => {
+      //main card 
+      const card = document.createElement('div')
+      card.classList.add('card')
+
+      //is the trio content
       const f = document.createElement('div')
       f.classList.add('palette')
 
-      f.style.backgroundImage = `linear-gradient(to right, ${fav.colors[0]}, ${fav.colors[1]}, ${fav.colors[2]})`
+      // Set background color for each color in the palette
+      fav.colors.forEach(color => {
+        const colorBlock = document.createElement('div');
+        colorBlock.style.backgroundColor = color;
+        colorBlock.classList.add('color-block');
+        f.appendChild(colorBlock);
+      });
+
+      // f.style.backgroundImage = `linear-gradient(to right, ${fav.colors[0]}, ${fav.colors[1]}, ${fav.colors[2]})`
       // f.style.width = '150px'
 
       //just for testing purposes
-      f.style.height = '100px'
+      // f.style.height = '100px'
 
-      palettes.appendChild(f)
+      // const swatch = document.createElement('div')
+      // swatch.classList.add('sample__swatch')
+
+      const paletteTitle = document.createElement('h3');
+      paletteTitle.textContent = fav.title; // Use the global variable
+
+      // place card inside main wrapper
+      palettes.appendChild(card)
+
+      
+      // place the trio colors into the card
+      card.appendChild(f)
+      // card.appendChild(swatch)
+
+      // // Append title inside the palette div
+      // f.appendChild(paletteTitle)
+
+      // Append title to card
+      card.appendChild(paletteTitle);
+
+  
+  
+
     })
   }
 }
